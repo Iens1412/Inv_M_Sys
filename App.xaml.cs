aea9f785bc64958a1654e76f3b921529b64d99d9
Mohammed <mohammad.kear.sy@gmail.com>
2025-01-24 23:50:54 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Inv_M_Sys
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
