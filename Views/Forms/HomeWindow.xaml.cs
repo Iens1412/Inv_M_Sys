c6dadf4ee5c4dc6c1a6405e880cac127f823b727
Mohammed Rezki <mohammad.kear.sy@gmail.com>
2025-04-17 23:37:00 +0200

    fixed animations

--- Views/Forms/HomeWindow.xaml.cs
+++ Views/Forms/HomeWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Threading;
 using Inv_M_Sys.Services;
 using Inv_M_Sys.Views.Main;
@@ -30,6 +32,12 @@ namespace Inv_M_Sys.Views.Forms
 
             // Navigate to the DashboardPage when HomeWindow is loaded
             NavigateToPage(new Views.Main.DashboardPage(this));
+
+            MainFrame.ContentRendered += (s, e) =>
+            {
+                var fade = new System.Windows.Media.Animation.DoubleAnimation(0.8, 1, TimeSpan.FromSeconds(0.8));
+                (MainFrame.Content as Page).BeginAnimation(OpacityProperty, fade);
+            };
 
             // Set up session timer that checks every minute
             _sessionTimer = new DispatcherTimer
@@ -67,15 +75,128 @@ namespace Inv_M_Sys.Views.Forms
         #region Navigation Methods
 
         /// <summary>
-        /// Navigates to the provided page within the HomeWindow.
+        /// Navigates to the provided page within the HomeWindow. with multiple animations
         /// </summary>
         /// <param name="page">The page to navigate to.</param>
         public void NavigateToPage(Page page)
         {
-            // Check if the page is already being displayed
             if (MainFrame.Content != page)
             {
-                MainFrame.NavigationService.Navigate(page);
+                // Avoid complete transparency to reduce brightness discomfort
+                page.Opacity = 0.8;
+                page.Margin = new Thickness(0);
+
+                MainFrame.Navigate(page);
+
+                page.Loaded += (s, e) =>
+                {
+                    var rand = new Random();
+                    int animationType = rand.Next(0, 8);
+
+                    var duration = TimeSpan.FromSeconds(2); // Slowed down
+
+                    switch (animationType)
+                    {
+                        case 0: // Fade in
+                            var fade = new DoubleAnimation(0.5, 1, duration)
+                            {
+                                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseOut }
+                            };
+                            page.BeginAnimation(Page.OpacityProperty, fade);
+                            break;
+
+                        case 1: // Slide from left
+                            var slideFromLeft = new ThicknessAnimation
+                            {
+                                From = new Thickness(-this.Width, 0, this.Width, 0),
+                                To = new Thickness(0),
+                                Duration = duration,
+                                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseOut }
+                            };
+                            page.BeginAnimation(MarginProperty, slideFromLeft);
+                            break;
+
+                        case 2: // Slide from right
+                            var slideFromRight = new ThicknessAnimation
+                            {
+                                From = new Thickness(this.Width, 0, -this.Width, 0),
+                                To = new Thickness(0),
+                                Duration = duration,
+                                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseOut }
+                            };
+                            page.BeginAnimation(MarginProperty, slideFromRight);
+                            break;
+
+                        case 3: // Zoom (scale)
+                            var scaleTransform = new ScaleTransform(0.9, 0.9);
+                            page.RenderTransformOrigin = new Point(0.5, 0.5);
+                            page.RenderTransform = scaleTransform;
+
+                            var scaleAnim = new DoubleAnimation(0.9, 1, duration)
+                            {
+                                EasingFunction = new CubicEase { EasingMode = EasingMode.EaseOut }
+                            };
+
+                            scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnim);
+                            scaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnim);
+
+                            var fadeIn = new DoubleAnimation(0.5, 1, duration);
+                            page.BeginAnimation(Page.OpacityProperty, fadeIn);
+                            break;
+
+                        case 4: // Slide from bottom
+                            var slideFromBottom = new ThicknessAnimation
+                            {
+                                From = new Thickness(0, this.Height, 0, -this.Height),
+                                To = new Thickness(0),
+                                Duration = duration,
+                                EasingFunction = new CircleEase { EasingMode = EasingMode.EaseOut }
+                            };
+                            page.BeginAnimation(MarginProperty, slideFromBottom);
+                            break;
+
+                        case 5: // Slide from top
+                            var slideFromTop = new ThicknessAnimation
+                            {
+                                From = new Thickness(0, -this.Height, 0, this.Height),
+                                To = new Thickness(0),
+                                Duration = duration,
+                                EasingFunction = new CircleEase { EasingMode = EasingMode.EaseOut }
+                            };
+                            page.BeginAnimation(MarginProperty, slideFromTop);
+                            break;
+
+                        case 6: // Rotate in
+                            var rotate = new RotateTransform(0);
+                            page.RenderTransformOrigin = new Point(0.5, 0.5);
+                            page.RenderTransform = rotate;
+
+                            var rotateAnim = new DoubleAnimation(0, 360, duration)
+                            {
+                                EasingFunction = new CircleEase { EasingMode = EasingMode.EaseOut }
+                            };
+                            rotate.BeginAnimation(RotateTransform.AngleProperty, rotateAnim);
+
+                            var fadeInWithRotate = new DoubleAnimation(0.5, 1, duration);
+                            page.BeginAnimation(Page.OpacityProperty, fadeInWithRotate);
+                            break;
+
+                        case 7: // Flip in horizontally
+                            var flip = new ScaleTransform(0.5, 1);
+                            page.RenderTransformOrigin = new Point(0.5, 0.5);
+                            page.RenderTransform = flip;
+
+                            var flipAnim = new DoubleAnimation(0.5, 1, duration)
+                            {
+                                EasingFunction = new CubicEase { EasingMode = EasingMode.EaseOut }
+                            };
+                            flip.BeginAnimation(ScaleTransform.ScaleXProperty, flipAnim);
+
+                            var flipFade = new DoubleAnimation(0.5, 1, duration);
+                            page.BeginAnimation(Page.OpacityProperty, flipFade);
+                            break;
+                    }
+                };
             }
         }
 
